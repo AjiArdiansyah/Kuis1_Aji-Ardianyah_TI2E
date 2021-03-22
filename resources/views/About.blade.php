9d45aec7f67bf8e1470e3c5acbc071a6d793a9c4
AjiArdiansyah <ajiardiansyah18@gmail.com>
2021-03-23 06:49:16 +0700

    p

---
+++ resources/views/About.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.Header')
+
+
+<!doctype html>
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+
+
+</head>
+<body>
+
+    @section('content')
+
+    @include('layouts.contentAbout')
+
+    @endsection
+
+    @section('footer')
+    @include('layouts.footer')
+
+    @endsection
+
+
+
+    </div><!-- /#right-panel -->
+
+
+</body>
+</html>
---
+++ resources/views/layouts/content.blade.php
@@ -0,0 +1,174 @@
+<!-- /////////////////////////////////////////Content -->
+<div id="page-content">
+	
+  
+  
+  <!-- ////////////Content Box 02 -->
+  <section class="box-content box-2" id="services">
+    <div class="container">
+      <div class="row heading">
+         <div class="col-lg-12">	
+                    <h2>TOP HOLIDAY DESTINATIONS</h2>
+                    <hr>
+                    <div class="intro">Lorem ipsum dolor sit amet</div>
+                </div>
+      </div>
+      <div class="categories">
+        <ul class="cat">
+        <li>
+          <ol class="type list-inline">
+            <li><a href="#" data-filter="*" class="active">All</a></li>
+            <li><a href="#" data-filter=".lake_wanaka">Lake Wanaka</a></li>
+            <li><a href="#" data-filter=".ohama_beach">Ohama Beach</a></li>
+            <li><a href="#" data-filter=".taupo_central">Taupo Central</a></li>
+          </ol>
+        </li>
+        </ul>
+        <div class="clearfix"></div>
+      </div>
+      
+      <div class="row">
+        <div class="portfolio-items">
+          <div class="col-sm-6 col-md-3 col-lg-3 taupo_central">
+            <div class="post">
+              <div class="item-container wow fadeInUp" data-wow-delay="200ms">
+                <div class="item-caption">
+                  <div class="item-caption-inner">
+                    <div class="item-caption-inner1">
+                      <a class="example-image-link" href="images/2.jpg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+                        <i class="fa fa-search"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <img src="images/2-thumb.jpg" />
+              </div>
+              <span>Aenean feugiat in ante et blandit. Vestibulum posuere molestie risus, ac interdum magna porta non.</span>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3 col-lg-3 taupo_central">
+            <div class="post">
+              <div class="item-container wow fadeInUp" data-wow-delay="200ms">
+                <div class="item-caption">
+                  <div class="item-caption-inner">
+                    <div class="item-caption-inner1">
+                      <a class="example-image-link" href="images/1.jpg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+                        <i class="fa fa-search"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <img src="images/1-thumb.jpg" />
+              </div>
+              <span>Aenean feugiat in ante et blandit. Vestibulum posuere molestie risus, ac interdum magna porta non.</span>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3 col-lg-3 taupo_central">
+            <div class="post">
+              <div class="item-container wow fadeInUp" data-wow-delay="200ms">
+                <div class="item-caption">
+                  <div class="item-caption-inner">
+                    <div class="item-caption-inner1">
+                      <a class="example-image-link" href="images/2.jpg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+                        <i class="fa fa-search"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <img src="images/2-thumb.jpg" />
+              </div>
+              <span>Aenean feugiat in ante et blandit. Vestibulum posuere molestie risus, ac interdum magna porta non.</span>
+            </div>					
+          </div>
+          <div class="col-sm-6 col-md-3 col-lg-3 lake_wanaka">
+            <div class="post">
+              <div class="item-container wow fadeInUp" data-wow-delay="200ms">
+                <div class="item-caption">
+                  <div class="item-caption-inner">
+                    <div class="item-caption-inner1">
+                      <a class="example-image-link" href="images/3.jpg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+                        <i class="fa fa-search"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <img src="images/3-thumb.jpg" />
+              </div>
+              <span>Aenean feugiat in ante et blandit. Vestibulum posuere molestie risus, ac interdum magna porta non.</span>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3 col-lg-3 lake_wanaka">
+            <div class="post">
+              <div class="item-container wow fadeInUp" data-wow-delay="200ms">
+                <div class="item-caption">
+                  <div class="item-caption-inner">
+                    <div class="item-caption-inner1">
+                      <a class="example-image-link" href="images/4.jpg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+                        <i class="fa fa-search"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <img src="images/4-thumb.jpg" />
+              </div>
+              <span>Aenean feugiat in ante et blandit. Vestibulum posuere molestie risus, ac interdum magna porta non.</span>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3 col-lg-3 lake_wanaka">
+            <div class="post">
+              <div class="item-container wow fadeInUp" data-wow-delay="200ms">
+                <div class="item-caption">
+                  <div class="item-caption-inner">
+                    <div class="item-caption-inner1">
+                      <a class="example-image-link" href="images/1.jpg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+                        <i class="fa fa-search"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <img src="images/1-thumb.jpg" />
+              </div>
+              <span>Aenean feugiat in ante et blandit. Vestibulum posuere molestie risus, ac interdum magna porta non.</span>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3 col-lg-3 ohama_beach">
+            <div class="post">
+              <div class="item-container wow fadeInUp" data-wow-delay="200ms">
+                <div class="item-caption">
+                  <div class="item-caption-inner">
+                    <div class="item-caption-inner1">
+                      <a class="example-image-link" href="images/3.jpg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+                        <i class="fa fa-search"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <img src="images/3-thumb.jpg" />
+              </div>
+              <span>Aenean feugiat in ante et blandit. Vestibulum posuere molestie risus, ac interdum magna porta non.</span>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3 col-lg-3 ohama_beach">
+            <div class="post">
+              <div class="item-container wow fadeInUp" data-wow-delay="200ms">
+                <div class="item-caption">
+                  <div class="item-caption-inner">
+                    <div class="item-caption-inner1">
+                      <a class="example-image-link" href="images/4.jpg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+                        <i class="fa fa-search"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <img src="images/4-thumb.jpg" />
+              </div>
+              <span>Aenean feugiat in ante et blandit. Vestibulum posuere molestie risus, ac interdum magna porta non.</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  
+</div>
